d1b7aa43f5163f19c2505435941f624b2182c1da
waqarali56 <2022cs666@uet.edu.pk>
2024-07-26 16:40:18 +0500

    first committ

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './layout.css'
+import Header from './header'
+import Footer from './Footer'
 
 export default function Layout() {
   return (
     <div>
-      
+      <Header/>
+      <Footer/>
     </div>
   )
 }
